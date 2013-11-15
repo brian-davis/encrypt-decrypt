e1ef124d1833bc0ab367369834252f9c52e8d247
Brian Davis <contact@briandavis.name>
2013-11-15 14:53:25 -0800

    updated Jasmine test

--- spec/encoderSpec.js
+++
@@ -1,8 +0,0 @@
-describe("Decoder", function(){
-  it("encodes a string to a URL", function() {
-    //URL access and user interaction mockup
-    encoder.setMessage();
-    encoder.getPassword();
-    expect(encoder.encrypt()).toEqual("Your encrypted message is available to anyone with the passphrase at: \n\n" + this.newLocation);
-  });
-});
