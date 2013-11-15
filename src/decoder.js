758d37a26661e5fa2529127c7a2e5e511635f290
Brian Davis <contact@briandavis.name>
2013-11-15 14:51:45 -0800

    update

--- src/decoder.js
+++ src/decoder.js
@@ -1,31 +1,22 @@
-var decoder = {
-  // promptPassword: function() {
-  //   document.getElementById("modal-wrapper").style.visibility="visible";
-  // },
-  // getPassword: function() {
-  //   this.password = document.getElementById("password-field").value;
-  //   document.getElementById("modal-wrapper").style.visibility="hidden";
-  //   this.getCypherText();
-  // },
-  // getCypherText: function() {
-  //   this.cypherText = window.location.search.substr(1);//split('?')[1]
-  //   this.performDecryption();
-  // },
-  getPassword : function() { decoder.password = "password"; },//test
-  getCypherText : function() { decoder.cypherText = CryptoJS.AES.encrypt("a test string", "password");},//test
-  performDecryption: function() {
-    this.decryptedMessage = "The message is: \n" + CryptoJS.AES.decrypt(decoder.cypherText, decoder.password).toString(CryptoJS.enc.Utf8) + ".";
-    return this.decryptedMessage;
-    this.displayMessage();
-  },
-  displayMessage: function() {
-    document.getElementById("message").value = this.decryptedMessage;
-  }
-};
-
-
-window.onload = function() {
-  // decoder.promptPassword();
-  decoder.getPassword();
-  decoder.getCypherText();
-};
+  var decoder = {
+      promptPassword: function() {
+        document.getElementById("modal-wrapper").style.visibility="visible";
+      },
+      getPassword: function() {
+        this.password = document.getElementById("password-field").value;
+        document.getElementById("modal-wrapper").style.visibility="hidden";
+        this.getHashFromUrl();
+      },
+      getHashFromUrl: function() {
+        this.hashFromUrl = window.location.search.substr(1);//split('?')[1]
+        this.performDecryption();
+      },
+      performDecryption: function() {
+        this.decryptedMessage = "The message is: \n" + CryptoJS.AES.decrypt(this.hashFromUrl, this.password).toString(CryptoJS.enc.Utf8) + ".";
+        this.displayMessage();
+      },
+      displayMessage: function() {
+        document.getElementById("message").value = this.decryptedMessage;
+      }
+    };
+    window.onload = decoder.promptPassword();
